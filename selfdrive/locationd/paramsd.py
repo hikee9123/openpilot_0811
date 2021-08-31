01e525d61e3a4673e6f94bd1773c0226e157d08e
hikee9123 <hikee9123@gmail.com>
2021-09-01 08:43:23 +0900

    modelSpeed dRate 450:0.8

--- selfdrive/locationd/paramsd.py
+++ selfdrive/locationd/paramsd.py
@@ -158,7 +158,7 @@ def main(sm=None, pm=None):
  
       dRate = 1.0
       if modelSpeed:
-        dRate = interp( modelSpeed, [200,350], [ 1, 0.9 ] )
+        dRate = interp( modelSpeed, [200,450], [ 1, 0.8 ] )
 
       steerRatio  = float(x[States.STEER_RATIO])
      # print( "modelSpeed={:.3f} dRate={:.3f}".format(modelSpeed, dRate) );
