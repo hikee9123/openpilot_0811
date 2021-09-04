729b16d6899e445faf2636121615b5cd3c9f34e4
hikee9123 <hikee9123@gmail.com>
2021-09-04 19:53:25 +0900

    panda

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -16,7 +16,7 @@
 #include "safety/safety_nissan.h"
 #include "safety/safety_volkswagen.h"
 #include "safety/safety_elm327.h"
-#include "safety/safety_hyundai_community.h"
+#include "safety/safety_hyundai_community2.h"
 
 // from cereal.car.CarParams.SafetyModel
 #define SAFETY_SILENT 0U
