76319d318c5b2a4db1344c1c4b4268bec4580724
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-09-23 10:13:52 +0900

    Update navicontrol.py

--- selfdrive/car/hyundai/navicontrol.py
+++ selfdrive/car/hyundai/navicontrol.py
@@ -184,7 +184,7 @@ class NaviControl():
 
       #if CS.cruise_set_mode == 1:
       if CS.cruise_set_mode == 1:
-        if( CS.out.gasPressed )
+        if CS.out.gasPressed:
           self.ctrl_speed = max( self.ctrl_speed, CS.clu_Vanz )
         #kph_fwd_speed = self.get_forword_car_speed( CS,  cruiseState_speed)
         #self.ctrl_speed = min( self.ctrl_speed, kph_fwd_speed)
