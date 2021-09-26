36ddb2e051d20d1b5c0b68d0beae0719b1a0bda8
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2021-09-26 12:13:27 +0900

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -267,7 +267,7 @@ class CarInterface(CarInterfaceBase):
     if candidate in [CAR.HYUNDAI_GENESIS, CAR.IONIQ_EV_2020, CAR.IONIQ_EV_LTD, CAR.IONIQ_PHEV, CAR.IONIQ, CAR.KONA_EV, CAR.KIA_SORENTO,
                      CAR.SONATA_LF, CAR.KIA_NIRO_EV, CAR.KIA_OPTIMA, CAR.VELOSTER, CAR.KIA_STINGER,
                      CAR.GENESIS_G70, CAR.GENESIS_G80, CAR.KIA_CEED, CAR.ELANTRA,
-                     CAR.GRANDEUR_HEV_19]:
+                     CAR.GRANDEUR_HEV_19,  CAR.GRANDEUR_HEV_20 ]:
       ret.safetyModel = car.CarParams.SafetyModel.hyundaiLegacy
 
       ret.safetyModel = car.CarParams.SafetyModel.hyundaiCommunity
